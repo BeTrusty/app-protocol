c589c08e7b9336c742dc89ef41297c489638bda4
Franco Carballar <francocarballar@gmail.com>
2024-08-30 05:44:25 -0300

    feat: :sparkles: agrega las URLs como constantes

---
+++ src/constants/url.ts
@@ -0,0 +1,40 @@
+/**
+ * @constant {string} API_COUNTRY_CODE
+ * @description Código de país para la API de Mercado Libre
+ * @default 'https://api.country.is/'
+ */
+export const API_COUNTRY_CODE: string = 'https://api.country.is/'
+
+/**
+ * @constant {string} API_CREATE_CREDENTIALS
+ * @description URL para crear credenciales
+ */
+export const API_CREATE_CREDENTIALS: string =
+  'https://sandbox-ssi.extrimian.com/v1/credentialsbbs/wacioob'
+
+/**
+ * @constant {string} API_CREATE_DID
+ * @description URL para crear un DID
+ */
+export const API_CREATE_DID: string =
+  'https://sandbox-ssi.extrimian.com/v1/dids/quarkid'
+
+/**
+ * @constant {string} API_GET_GITHUB_USER
+ * @description URL para obtener información de un usuario de GitHub
+ */
+export const API_GET_GITHUB_USER: string = 'https://api.github.com/users/'
+
+/**
+ * @constant {string} API_GET_MERCADO_LIBRE_USER
+ * @description URL para obtener información de un usuario de Mercado Libre
+ */
+export const API_GET_MERCADO_LIBRE_USER: string =
+  'https://api.mercadolibre.com/users/'
+
+/**
+ * @constant {string} API_GET_USER
+ * @description URL para obtener información de un usuario
+ */
+export const API_GET_USER: string =
+  'https://api-betrusty.vercel.app/user/all?id_user='
