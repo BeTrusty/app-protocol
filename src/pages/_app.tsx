8e6fcdffd27de2f521be1d431de5a935a052bd62
Franco Carballar <francocarballar@gmail.com>
2024-08-30 06:06:11 -0300

    feat: :sparkles: actualiza el archivo _app.tsx para agregar el provider del context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Provider } from '@/context'
+import { NextUIProvider } from '@nextui-org/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App ({ Component, pageProps }: AppProps) {
+  return (
+    <Provider>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </Provider>
+  )
 }
