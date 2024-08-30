2deec07f268f38114ba7b877a633193c1f5a6ba5
Franco Carballar <francocarballar@gmail.com>
2024-08-30 05:39:10 -0300

    feat: :sparkles: agrega los iconos y logos al proyecto

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,27 @@
+import { IconWelcome } from './IconWelcome'
+import { IconFingerprint } from './IconFingerprint'
+import { IconGetData } from './IconGetData'
+import { IconPassport } from './IconPassport'
+import { LogoMercadoLibre } from './LogoMercadoLibre'
+import { LogoGithub } from './LogoGithub'
+import { LogoBCRA } from './LogoBCRA'
+import { LogoLinkedin } from './LogoLinkedin'
+import { LogoFacebook } from './LogoFacebook'
+import { LogoInstagram } from './LogoInstagram'
+import { LogoAirbnb } from './LogoAirbnb'
+import { LogoQuarkID } from './LogoQuarkId'
+
+export {
+  IconWelcome,
+  IconFingerprint,
+  IconGetData,
+  IconPassport,
+  LogoMercadoLibre,
+  LogoGithub,
+  LogoBCRA,
+  LogoLinkedin,
+  LogoFacebook,
+  LogoInstagram,
+  LogoAirbnb,
+  LogoQuarkID
+}
