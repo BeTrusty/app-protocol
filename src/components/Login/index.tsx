5668712f35c6b9bfd07d7a026b4da1199878a516
John Sebastián Ceciliano Piedra <jsebascp04@gmail.com>
2024-12-13 20:09:48 -0300

    feat:button add and image
    - Se agrego boton y imagen

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,10 +1,15 @@
-import Image from 'next/image'
-import { Title } from '@components/Title'
-import { Button } from '@nextui-org/react'
+import React from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import { Button } from '@nextui-org/react' // Importa solo el componente Button
+import { Title } from '@components/Title' // Importa solo el componente Button
 
 export function Login (): JSX.Element {
-  const endpointApi =
-    'https://id.worldcoin.org/authorize?response_type=code&redirect_uri=https://api-betrusty.vercel.app/worldid/callback&scope=openid+profile+email&client_id=app_b5bf70a63e4ecd0be5f1b754b6675728'
+  const router = useRouter();
+  const endpointApiWorldID =
+    'https://id.worldcoin.org/authorize?response_type=code&redirect_uri=https://api-betrusty.vercel.app/worldid/callback&scope=openid+profile+email&client_id=app_b5bf70a63e4ecd0be5f1b754b6675728';
+  const endpointApiZkSync =
+    'https://zksync.io/authorize?response_type=code&redirect_uri=https://api-betrusty.vercel.app/zksync/callback&scope=openid+profile+email&client_id=your_zksync_client_id';
 
   /**
    * @function signInWithWorldID
@@ -12,14 +17,23 @@ export function Login (): JSX.Element {
    * @returns {void}
    */
   const signInWithWorldID = (): void => {
-    window.location.href = endpointApi
-  }
+    window.location.href = endpointApiWorldID;
+  };
+
+  /**
+   * @function signInWithZkSync
+   * @description Función para redirigir al usuario a la página de zkSync
+   * @returns {void}
+   */
+  const signInWithZkSync = (): void => {
+    window.location.href = endpointApiZkSync;
+  };
 
   return (
     <section className='flex flex-col justify-start items-center w-full max-w-[400px] mt-5 p-5'>
       <Title text='Inicia sesión' />
       <Image
-        src='/img/worldId.avif'
+        src='/img/SSO.png'
         width={400}
         height={500}
         alt='Imagen del pasaporte de WorldID'
@@ -32,6 +46,14 @@ export function Login (): JSX.Element {
       >
         Conectar WorldID
       </Button>
+      <Button
+        color='primary'
+        radius='full'
+        className='w-full max-w-[260px] mt-4'
+        onClick={signInWithZkSync}
+      >
+        Conectar zkSync
+      </Button>
     </section>
-  )
+  );
 }
