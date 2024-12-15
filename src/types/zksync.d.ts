134ba0179b46af7e16915d4e8b80e8f796ad6bb2
Franco Carballar <francocarballar@gmail.com>
2024-12-15 04:05:31 -0300

    feat: :label: actualiza el tipado de zksync

--- src/types/zksync.d.ts
+++ src/types/zksync.d.ts
@@ -2,3 +2,12 @@ export interface Token {
   address: `0x${string}`
   decimals: number
 }
+
+export interface ZkSyncProof {
+  userAddress: string
+  isValid: boolean // Ejemplo: indica si la prueba pasó la validación
+}
+
+export interface ZkSyncUser {
+  userId: string
+}
