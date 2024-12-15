e9586c471fe35cabfce1a830cf343143587a1380
Franco Carballar <francocarballar@gmail.com>
2024-12-15 09:20:05 -0300

    feat: :sparkles: integra talent protocol al proyecto

--- src/hooks/useConnectInformation.tsx
+++ src/hooks/useConnectInformation.tsx
@@ -1,59 +1,75 @@
-import { CredentialData, CredentialSubject, ResponseVCData } from "@/types/credentials"
-import { useState } from "react"
-import { useLocalStorage } from "./useLocalStorage"
-
+import {
+  CredentialData,
+  CredentialSubject,
+  ResponseVCData
+} from '@/types/credentials'
+import { useEffect, useState } from 'react'
+import { useLocalStorage } from './useLocalStorage'
+import { createTalentProtocol } from '@/firebase/talentProtocol/create'
+import { TalentProtocol } from '@/types/talentProtocol'
 
 export function useConnectInformation () {
-    const [localId, setLocalId] = useLocalStorage<string>('localId')
-    const [level, setLevel] = useLocalStorage<string>('level')
-    const [didUrl, setDidUrl] = useLocalStorage<string>('didUrl')
-    const [time, setTime] = useLocalStorage<number>('time')
-    const [redes, setRedes] = useState<{talentProtocol:boolean, mercadoLibre:boolean, github:boolean, autopen:boolean}>(
-      {talentProtocol:false, mercadoLibre:false, github:false, autopen:false}
-    )
-    const [talentProtocol, setTalentProtocol] = useState<{}>({})
-    const [autopen, setAutopen] = useState<{}>({})
-    const [credential, setCredential] =
-      useLocalStorage<CredentialData>('credential')
-    const [data, setData] = useState<ResponseVCData | null>(null)
-    const [loading, setLoading] = useState<boolean>(true)
-    const [isLoading, setIsLoading] = useState<{
-      github: boolean
-      mercadoLibre: boolean
-      talentprotocol: boolean
-    }>({
-      github: true,
-      mercadoLibre: true,
-      talentprotocol: true
-    })
-    const [error, setError] = useState<string | null>(null)
-    const [user, setUser] = useState<CredentialSubject | null>(null)
-  return { 
-    localId, 
+  const [localId, setLocalId] = useLocalStorage<string>('localId')
+  const [level, setLevel] = useLocalStorage<string>('level')
+  const [didUrl, setDidUrl] = useLocalStorage<string>('didUrl')
+  const [time, setTime] = useLocalStorage<number>('time')
+  const [redes, setRedes] = useState<{
+    talentProtocol: boolean
+    mercadoLibre: boolean
+    github: boolean
+    autopen: boolean
+  }>({
+    talentProtocol: false,
+    mercadoLibre: false,
+    github: false,
+    autopen: false
+  })
+  const [talentProtocol, setTalentProtocol] = useState<TalentProtocol | null>(
+    null
+  )
+  const [autopen, setAutopen] = useState<{}>({})
+  const [credential, setCredential] =
+    useLocalStorage<CredentialData>('credential')
+  const [data, setData] = useState<ResponseVCData | null>(null)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [isLoading, setIsLoading] = useState<{
+    github: boolean
+    mercadoLibre: boolean
+    talentprotocol: boolean
+  }>({
+    github: true,
+    mercadoLibre: true,
+    talentprotocol: true
+  })
+  const [error, setError] = useState<string | null>(null)
+  const [user, setUser] = useState<CredentialSubject | null>(null)
+
+  return {
+    localId,
     setLocalId,
-    level, 
+    level,
     setLevel,
-    didUrl, 
+    didUrl,
     setDidUrl,
-    time, 
+    time,
     setTime,
     redes,
     setRedes,
-    talentProtocol, 
+    talentProtocol,
     setTalentProtocol,
-    autopen, 
+    autopen,
     setAutopen,
-    credential, 
+    credential,
     setCredential,
-    data, 
+    data,
     setData,
-    loading, 
+    loading,
     setLoading,
-    isLoading, 
+    isLoading,
     setIsLoading,
-    error, 
+    error,
     setError,
-    user, 
+    user,
     setUser
-}
+  }
 }
