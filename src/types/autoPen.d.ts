2fcee312acdc518b32e16c989e487c2e8ffe0894
Franco Carballar <francocarballar@gmail.com>
2024-12-16 16:14:30 -0300

    feat: :label: actualiza el tipado de autopen

--- src/types/autoPen.d.ts
+++ src/types/autoPen.d.ts
@@ -1,15 +1,16 @@
-export interface AutoPen{
-    nft: {
-        tokenId: string
-        contractAddress:string
-        transactionHash: string
-    },
-    certificate: {
-        serialNumber: string
-        subject: string
-        issuer: string
-        validFrom:string
-        validTo: string
-        pemData: object
-    }
+export interface AutoPen {
+  userId: string
+  nft: {
+    tokenId: string
+    contractAddress: string
+    transactionHash: string
+  }
+  certificate: {
+    serialNumber: string
+    subject: string
+    issuer: string
+    validFrom: string
+    validTo: string
+    pemData: object
+  }
 }
